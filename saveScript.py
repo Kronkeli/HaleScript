65edad1d726ed7c013715be172f037b025779e9f
Samu Salko <samusalko@Samu-MacBook-Pro-3.local>
2021-03-22 00:52:14 +0200

    Changed DESTPATH variable to be read from .env file.

--- saveScript.py
+++ saveScript.py
@@ -2,37 +2,53 @@
 File which saves the script lines from development environment to
 actual script destination
 
-Params:
+Params: 
 - SRCPATH = path to the script file
-- DESTPATH = Name of the file (set to be)
+- DESTPATH = Name of the file (write the value of script path to .env file)
 """
 
-import sys
+import os
 
-# Set arguments to constant variables0
+# Setting the path variables for text files.
+#  -  !! Create an .env file and set the DESTPATH there. !!  -
 SRCPATH = "Sfscript\\Program.cs"
-DESTPATH = "C:\\Users\\samus\\Documents\\Superfighters Deluxe\\Scripts\\hale.txt"
+
+# 'Cooler Daniel' way to do it, but needs pip installing..
+# from dotenv import load_dotenv
+# load_dotenv()
+# DESTPATH = os.getenv('DESTPATH')
+
+# 'Cool Daniel'
+abort = False
+try:
+    envFile = open(".env", "r")
+    DESTPATH = envFile.read()
+    envFile.close()
+except:
+    print("*** File reading error. Check that .env file exists in the root directory. *** ")
+    abort = True
+
 
 def main():
-    srcfile = open(SRCPATH,"r")
-    
+    srcfile = open(SRCPATH, "r")
+
     rows = srcfile.readlines()
-    
+
     hasScriptBegun = False
     hasScriptEnded = False
 
     resultString = ""
-    
+
     # Loop through the source file. The script lines are specified between the marks '//hahaYES' and '//hahaNO'
     for row in rows:
-        if ( "//hahaNO" in row ):
-                hasScriptEnded = True
-        if (  not hasScriptEnded and not hasScriptEnded):
-            if ( hasScriptBegun ):
+        if ("//hahaNO" in row):
+            hasScriptEnded = True
+        if (not hasScriptEnded and not hasScriptEnded):
+            if (hasScriptBegun):
                 resultString = resultString + row
             else:
                 # Check if script has started so reading can begin
-                if ( "//hahaYES" in row ):
+                if ("//hahaYES" in row):
                     hasScriptBegun = True
             # Check if script has ended so reading can stop
     srcfile.close()
@@ -40,5 +56,8 @@ def main():
     # In the endsave the resulting string to the new file
     destfile = open(DESTPATH, "w")
     destfile.write(resultString)
+    destfile.close()
 
-main()
+
+if(not abort):
+    main()
